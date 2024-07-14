d89a4945210a7bfb34cc0a43f5276c1dd7296498
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-07-14 10:08:13 +0200

    Added footer with links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Dictionary</h1>
+      <footer>
+        This project was coded by Sarah Gray. It is open-sourced on{" "}
+        <a href="https://github.com/SL-GW/dictionary-project" target="blank">
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://dictionary-project-using-react.netlify.app/"
+          target="blank"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
