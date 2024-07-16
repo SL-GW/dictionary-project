d15f022dfc845bda9afdd7699d2a73430d7adf32
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-07-16 09:05:13 +0200

    Added more to the search engine

--- src/Results.js
+++ src/Results.js
@@ -3,9 +3,31 @@ import React from "react";
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
+    const { partOfSpeech, definition, example, synonyms, antonyms } =
+      props.results;
     return (
       <div className="Results">
-        <h2>{props.results.definition}</h2>
+        <div>
+          <h3>{partOfSpeech}</h3>
+          <p>
+            <strong>Definition:</strong> {definition}
+          </p>
+          {example && (
+            <p>
+              <strong>Example:</strong> {example}
+            </p>
+          )}
+          {synonyms && synonyms.length > 0 && (
+            <p>
+              <strong>Synonyms:</strong> {synonyms.join(", ")}
+            </p>
+          )}
+          {antonyms && antonyms.length > 0 && (
+            <p>
+              <strong>Antonyms:</strong> {antonyms.join(", ")}
+            </p>
+          )}
+        </div>
       </div>
     );
   } else {
